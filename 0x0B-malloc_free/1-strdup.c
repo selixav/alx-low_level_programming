8c0a6b6dbf1765c271b450434377eaeb69ba81d9
root <selinaxavier6@gmail.com>
2022-04-04 11:59:06 -0700

    moid

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,7 +12,8 @@ int i = 0, size = 0;
 char *m;
 if (str == NULL)
 return (NULL);
-for (; str[size] != '\0'; size++);
+for (; str[size] != '\0'; size++)
+;
 /*+1 on the size puts the end of string character*/
 m = malloc(size * sizeof(*str) + 1);
 if (m == 0)
@@ -21,6 +22,6 @@ else
 {
 for (; i < size; i++)
 m[i] = str[i];
-}	
+}
 return (m);
 }
